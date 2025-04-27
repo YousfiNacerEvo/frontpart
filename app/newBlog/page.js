451eee1a53e2061709401b504d565be6afd39ba7
YousfiNacerEvo <yousfinacer270@gmail.com>
2025-04-27 19:42:26 +0100

    update of url

--- app/newBlog/page.js
+++ app/newBlog/page.js
@@ -10,7 +10,7 @@ export default function newBlog() {
 
   async function submit(){
     try{
-      let res =await fetch('http://localhost:5000/newblog', {
+      let res =await fetch('https://back-12ec.onrender.com/newblog', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
